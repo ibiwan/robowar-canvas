a4f120e1edcb1755ce06c462e3b5c91809abe315
Jeremy Kent <jkent@paylease.com>
2020-10-20 23:39:17 -0700

    working brain (simple case)

--- brain.js
+++ brain.js
@@ -1,3 +1,109 @@
+const operators = ['ABS', 'AND', 'CALL', 'CHS'/*unary negation*/, 'DIST', 
+  'DROP', 'DROPALL', 'DUP', 'IF', 'IFE', 'IFG', 'JUMP', 'MAX', 'MIN', 'MOD', 
+  'NOP', 'NOT', 'OR', 'RETURN', 'STO', 'STORE', 'SWAP', 'SYNC', 'XOR', 
+  '+', '-'/*subtract*/, '*', '/', '=', '!' /*not equal*/, '>', '<'
+];
+
+const ro_operands = [
+  'CHRONON', 'ID', 'RADAR', 'RANGE', 'DOPPLER', 'RANDOM', 'WALL',
+];
+
+const rw_operands = [
+  'AIM', 'SHIELD', 'SPEEDX', 'SPEEDY',
+];
+
+const wo_operands = [
+  'BULLET', 'MOVEX', 'MOVEY', 
+];
+
+const ACODE = 'A'.charCodeAt(0);
+for(let i = 0; i < 26; i++){
+  rw_operands.push(String.fromCharCode(ACODE+i))
+}
+
+const keywords = [
+  ...operators, 
+  ...ro_operands,
+  ...rw_operands,
+  ...wo_operands,
+]
+console.log({keywords})
+
+const r_operands = [...ro_operands, ...rw_operands];
+const w_operand_names = [...rw_operands, ...wo_operands].map(s=>s+"'")
+
+console.log({w_operand_names})
+
+const getNewStack = () => {
+  const internal = Symbol();
+  
+  const NUMBER_T = 'number';
+  const OPERAND_NAME = 'operand_name';
+
+  const newStack = [];
+
+  newStack.push = function(id, val){
+    if(id !== internal){
+      throw new Error('don\'t call stack.push directly; use pushNumber or pushTarget');
+    }
+    return Array.prototype.push.call(this, val);
+  }
+
+  newStack.pushNumber = function(val){
+    if(!Number.isInteger(val)){
+      throw new Error('attempted to push value as number: ' + val);
+    }
+    this.push(internal, {
+      type: NUMBER_T,
+      value: val,
+    })
+  }
+
+  newStack.pushTarget = function(val){
+    if(!val in w_operand_names){
+      throw new Error('attempted to push value as target operand: ' + val);
+    }
+    this.push(internal, {
+      type: OPERAND_NAME,
+      value: val
+    })
+  }
+  
+  newStack.pop = function(id){
+    if(id !== internal){
+      throw new Error('don\'t call stack.pop directly; use popNumber or popTarget');
+    }
+    return Array.prototype.pop.call(this);
+  }
+
+  newStack.assertHas = function(){
+    if(this.length === 0){
+      throw new Error('stack underflow');
+    }
+  }
+
+  newStack.popNumber = function () {
+    this.assertHas();
+    const value = this.pop(internal);
+    if(value.type !== NUMBER_T){
+      throw new Error('tried to write a non-number to an operand: ' + value.value);
+    }
+    return value.value;
+  }
+
+  newStack.popTarget = function(){
+    this.assertHas();
+    const target = this.pop(internal);
+    if(target.type !== OPERAND_NAME){
+      throw new Error('tried to store to a non-operand: ' + target.value);
+    }
+    
+    return target.value;
+  }
+
+  return newStack;
+}
+
 const compile = (bot) => {
   const { rawCode } = bot;
 
@@ -12,7 +118,7 @@ const compile = (bot) => {
 
     // add line numbers for later debugging
     .map((src, lineNo) => ({ src, lineNo }))
-
+    
     // remove comments
     .map(({ src, ...rest }) => ({
       ...rest,
@@ -46,16 +152,20 @@ const compile = (bot) => {
     .reduce(
       ({ symbols, labels }, cur, idx) => {
         const { src } = cur;
-        const isLabel = src.startsWith(":");
 
-        if (isLabel) {
-          labels[src.slice(1)] = symbols.length; // idx
+        if (src.endsWith(":")) {
+          const label = src.slice(0, -1);
+          if(keywords.includes(label)){
+            throw new Error("INVALID LABEL:", label)
+          }
+          labels[label] = symbols.length; // idx
         } else {
           symbols.push(cur);
         }
 
         return { symbols, labels };
       },
+
       // reduce init
       {
         symbols: [],
@@ -67,11 +177,107 @@ const compile = (bot) => {
     ...bot,
     compiledCode,
     index: 0, // current instruction
-    stack: [], // RPN stack
+    stack: getNewStack(), // RPN stack
     queue: [], // instructions to be executed by game engine
   };
 };
 
+const isIntStr = str => !!str.match(/-?\d+/)
+
 const execChronon = (bot, environs) => {
+  const {
+    compiledCode: {
+      symbols: program, 
+      labels,
+    },
+    cpu, 
+    stack, 
+    queue,
+  } = bot;
+
+  let instrsExecuted = 0;
   const commands = [];
+
+  while(instrsExecuted < cpu){
+    const {src: currInstr} = program[bot.index];
+    
+    // console.log({index: bot.index, currInstr})
+    let incrementIndex = true;
+
+    switch(true){
+      case !currInstr:
+        throw new Error('invalid instruction index: ' + currInstr)
+        break;
+      case currInstr in labels:
+        const labelIndex = labels[currInstr];
+        stack.pushNumber(labelIndex);
+        break;
+      case isIntStr(currInstr):
+        stack.pushNumber(parseInt(currInstr))
+        break;
+      case operators.includes(currInstr):
+        switch(currInstr){
+          case '+':
+            stack.pushNumber(stack.popNumber() + stack.popNumber())
+            break;
+          case '=':
+            stack.pushNumber(stack.popNumber() === stack.popNumber() ? 1 : 0)
+            break;
+          case 'IFG': // 'IF' leaves a return address
+            {
+              const newIndex = stack.popNumber();
+              const predicate = stack.popNumber();
+              
+              if(predicate !== 0){
+                bot.index = newIndex;
+                incrementIndex = false;
+              }
+            }
+            break;
+          case 'JUMP': // 'CALL' leaves a return address
+            const newIndex = stack.popNumber();
+            bot.index = newIndex;
+            incrementIndex = false;
+            break;
+          case 'STO':
+          case 'STORE':
+            const target = stack.popTarget();
+            const value = stack.popNumber();
+            switch(target){
+              case "SPEEDX'":
+                bot.speedx = value;
+                break;
+              default:
+                throw new Error('don\'t know how to assign to operand: ' + target);
+            }
+            break;
+          default:
+            throw new Error('don\'t have implementation for operator: ' + currInstr);
+        }
+        break;
+      case w_operand_names.includes(currInstr):
+        stack.pushTarget(currInstr);
+        break;
+      case r_operands.includes(currInstr):
+        switch(currInstr){
+          case 'X':
+            stack.pushNumber(bot.x);
+            break;
+          default:
+            throw new Error('don\'t know how to read from operand: ' + currInstr);
+        }
+        break;
+      case currInstr === 'STO' || currInstr === 'STORE':
+      default:
+        throw new Error('unhandled situation! ' + currInstr)
+        break;
+    }
+
+    if(incrementIndex){
+      bot.index += 1;
+    }
+    instrsExecuted += 1;
+    
+    // console.log({stack: JSON.stringify(stack.map(v=>v.value))})
+  }
 };
--- game.js
+++ game.js
@@ -7,44 +7,62 @@ const botRadius = 16;
 
 const rawBots = [
   {
-    x: 20,
+    x: 200,
     y: 40,
-    vx: 2,
-    vy: 3,
+    speedx: 0,
+    speedy: 0,
     color: "#EE0000",
     aim: 0,
     dAim: 8,
-    cpu: 15,
-    rawCode:
-      "the #it's a definite article \n quick \r brown\tfox :jumps\r over\nthe\r\n:really lazy dog 8 12 'dog '12 dog8",
+    cpu: 5,
+    rawCode: `
+      Start:
+        -1 speedx' STORE
+      Loop:
+        0 x = LeftWall IFG
+        300 x = RightWall IFG
+        Loop JUMP
+      LeftWall:
+        1 speedx' STORE
+        Loop Jump
+      RightWall:
+        -1 speedx' STORE
+        Loop Jump
+      Stop:
+        AIM 7 + AIM' STORE
+        Stop JUMP
+    `
+    // parsertest:       "the #it's a definite article \n quick \r brown\tfox :jumps\r over\nthe\r\n:really lazy dog 8 12 'dog '12 dog8",
   },
-  {
-    x: 20,
-    y: 40,
-    vx: -1,
-    vy: -2,
-    color: "#00CC33",
-    aim: 180,
-    dAim: -12,
-    cpu: 10,
-    rawCode: "",
-  },
+  // {
+  //   x: 20,
+  //   y: 40,
+  //   speedx: -1,
+  //   speedy: -2,
+  //   color: "#00CC33",
+  //   aim: 180,
+  //   dAim: -12,
+  //   cpu: 10,
+  //   rawCode: "",
+  // },
 ];
 
 let bots;
 
-const updateDim = (x, vx) => {
-  x += vx;
-  if (x > arenaSide + botRadius) {
-    x = arenaSide + botRadius;
-    vx = -(Math.floor(Math.random() * 3) + 1);
+const updateDim = (x, speedx) => {
+  x += speedx;
+  if (x > arenaSide) {
+    x = arenaSide;
+    speedx = 0;
   }
-  if (x < botRadius) {
-    x = botRadius;
-    vx = Math.floor(Math.random() * 3) + 1;
+  if (x < 0) {
+    x = 0;
+    speedx = 0;
   }
 
-  return [x, vx];
+  // console.log([x, speedx])
+
+  return [x, speedx];
 };
 
 const init = () => {
@@ -57,35 +75,47 @@ const init = () => {
   setTimeout(update, 0);
 };
 
+const drawBot = (ctx, bot) => {
+  let { x, y, speedx, speedy, color, aim, dAim } = bot;
+
+  ctx.beginPath();
+  ctx.rect(x , y, 2 * botRadius, 2 * botRadius);
+  ctx.fillStyle = color;
+  ctx.fill();
+  ctx.closePath();
+
+  ctx.beginPath();
+  ctx.moveTo(x + botRadius, y+botRadius);
+  ctx.lineTo(
+    x + botRadius + botRadius * Math.cos((aim * Math.PI) / 180),
+    y + botRadius + botRadius * Math.sin((aim * Math.PI) / 180)
+  );
+  ctx.stroke();
+  ctx.closePath();
+}
+
 const update = () => {
   const startTime = new Date().getTime();
 
   ctx.clearRect(0, 0, canvas.width, canvas.height);
 
   bots.forEach((bot) => {
-    let { x, y, vx, vy, color, aim, dAim } = bot;
-
-    [x, vx] = updateDim(x, vx);
-    [y, vy] = updateDim(y, vy);
-    aim += dAim;
-
-    Object.assign(bot, { x, y, vx, vy, aim, dAim });
-
-    ctx.beginPath();
-    ctx.rect(x - botRadius, y - botRadius, 2 * botRadius, 2 * botRadius);
-    ctx.fillStyle = color;
-    ctx.fill();
-    ctx.closePath();
-
-    ctx.beginPath();
-    ctx.moveTo(x, y);
-    ctx.lineTo(
-      x + botRadius * Math.cos((aim * Math.PI) / 180),
-      y + botRadius * Math.sin((aim * Math.PI) / 180)
-    );
-    ctx.stroke();
-    ctx.closePath();
-  });
+    
+    execChronon(bot, {});
+    
+    // console.log({bot})
+    // throw new Error('purple')
+    
+    // console.log({x, speedx});
+    
+      let { x, y, speedx, speedy, aim, dAim } = bot;
+      [x, speedx] = updateDim(x, speedx);
+      [y, speedy] = updateDim(y, speedy);
+      aim += dAim;
+      Object.assign(bot, {x, speedx, y, speedy, aim, dAim})
+            
+      drawBot(ctx, bot);
+    });
 
   setTimeout(update, rate - (new Date().getTime() - startTime));
 };
